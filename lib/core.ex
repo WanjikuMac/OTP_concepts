a596cff347976b681d2b3e6799ce4e79fc1dfa94
WanjikuMac <edwardscaroline32@gmail.com>
2020-06-10 12:23:54 +0300

    use a negative for decrease

--- lib/core.ex
+++ lib/core.ex
@@ -5,7 +5,7 @@ defmodule Calculator.Core do
     def divide(acc, number), do: acc/number
 
     def inc(acc), do: acc + 1
-    def dec(acc), do: acc + 1
+    def dec(acc), do: acc - 1
 
     def fold(list, acc, f), do: Enum.reduce(list, acc, &(f.(&2, &1)))
 end
