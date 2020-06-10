33010c3ec46fa76a886ebf0b1cef1a58584fa02d
WanjikuMac <edwardscaroline32@gmail.com>
2020-06-10 11:41:28 +0300

    build the API layer with send

--- lib/calculator.ex
+++ lib/calculator.ex
@@ -1,18 +1,30 @@
 defmodule Calculator do
-  @moduledoc """
-  Documentation for `Calculator`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Calculator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  alias Calculator.Boundary
+  # This function returns a pid
+  def start(initial_state) do
+    Boundary.start(initial_state)
   end
+
+  #These functions are async
+  def add(calculator, n), do: send(calculator, {:add, n})
+  def subtract(calculator, n), do: send(calculator, {:subtract, n})
+  def multiply(calculator, n), do: send(calculator, {:multiply, n})
+  def divide(calculator, n), do: send(calculator, {:divide, n})
+
+  def custom(calculator, f, n), do: send(calculator, {:custom, f, n})
+
+  def clear(calculator), do: send(calculator, :clear)
+
+  #Outline of the sync function to retrieve state
+  def state(calculator) do
+    send(calculator, {:state, self()})
+    receive do
+      {:state, state} ->
+        state
+    after
+      5000 ->
+        {:error, :timeout}
+    end
+  end
+  
 end
--- test/calculator_test.exs
+++ test/calculator_test.exs
@@ -2,7 +2,5 @@ defmodule CalculatorTest do
   use ExUnit.Case
   doctest Calculator
 
-  test "greets the world" do
-    assert Calculator.hello() == :world
-  end
+
 end
